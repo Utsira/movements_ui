659f25c3de42d1fef58333f8427cb336ea263feb
Aidan Hamade <aidan.hamade@gmail.com>
2016-04-29 14:58:31 +0100

    remove some boilerplate

--- static/src/js/home.js
+++ static/src/js/home.js
@@ -1,26 +1,4 @@
-// const Backbone = require('backbone');
-//
-// const homePage = (function () {
-//   const HomeView = Backbone.View.extend({
-//     initialize: function () {
-//       console.log('calling home initialise');
-//     },
-//     test: function () {}
-//
-//   });
-//
-//   const init = function () {
-//     new HomeView();
-//   };
-//
-//   return {
-//     init: init
-//   };
-// })();
-//
-// module.exports = homePage;
-
-var React = require('react');
+const React = require('react');
 
 var App = React.createClass({
   render: function() {
--- static/src/js/site.js
+++ static/src/js/site.js
@@ -1,20 +1,8 @@
-// /* global window */
-// window.jQuery = window.$ = require('jquery');
-//
-// const $ = window.$;
-// const home = require('./home.js');
-//
-// require('bootstrap');
-//
-// window.movements = {};
-// window.movements.initHome = home.init;
-
 /* global window */
 window.jQuery = window.$ = require('jquery');
 const $ = window.$;
 
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-var App = require('./home.js');
-ReactDOM.render(<App/>, document.body);
+const React = require('react')
+const ReactDOM = require('react-dom');
+const App = require('./home.js');
+//ReactDOM.render(<App/>, document.body);
